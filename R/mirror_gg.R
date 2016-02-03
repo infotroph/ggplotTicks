2a197416b1a72a7a88b805507273d21e406e86d0
Chris Black <chris@bodger.org>
2016-02-02 20:04:28 -0600

    mirror_ticks: finally returns a ggplot object!

---
+++ R/mirror_gg.R
@@ -0,0 +1,17 @@
+# Add tick-mirroring to a ggplot object.
+# (Actually just subclass it so that the mirroring will happen at render time.)
+mirror_ticks = function(ggobj, allPanels=FALSE){
+	tcl = if(allPanels==TRUE){ "ggTicksAll" }else{ "ggTicks" }
+	class(ggobj$facet) = c(tcl, class(ggobj$facet))
+	ggobj
+}
+
+
+# S3 methods to be picked up by the ggplot2::facet_render generic
+facet_render.ggTicks = function(...){
+	mirror_gtable(NextMethod(facet_render))
+}
+
+facet_render.ggTicksAll = function(...){
+	mirror_gtable(NextMethod(facet_render), allPanels=TRUE)
+}
